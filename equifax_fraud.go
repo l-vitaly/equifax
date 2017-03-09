4b4516da068e0d664450f05c8545081e46dcacb6
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-03-09 16:18:02 +0300

    refactor(const.go,equifax_credit.go,equifax_fraud.go): rename const

--- equifax_fraud.go
+++ equifax_fraud.go
@@ -41,47 +41,47 @@ type NewApplication struct {
 	Birthplace                 EmptyString            `xml:"birthplace"`
 	DocType                    DocType                `xml:"doctype"`
 	DocNo                      string                 `xml:"docno"`
-	DocPlace                   EmptyString            `xml:"docplace"`
-	DocDate                    Date                   `xml:"docdate"`
-	DocCode                    EmptyString            `xml:"doccode"`
-	PastDocType                DocType                `xml:"pastdoctype"`
-	PastDocNo                  EmptyString            `xml:"pastdocno"`
-	PastDocPlace               EmptyString            `xml:"pastdocplace"`
-	PastDocDate                Date                   `xml:"pastdocdate"`
-	Sex                        Sex                    `xml:"sex"`
-	Citizenship                Citizenship            `xml:"citizenship"`
-	INN                        EmptyString            `xml:"inn"`
-	PFR                        EmptyString            `xml:"pfr"`
-	DriverNo                   EmptyString            `xml:"driverno"`
-	Education                  Education              `xml:"education"`
-	Marital                    Marital                `xml:"marital"`
-	NumChildren                EmptyString            `xml:"numchildren"`
-	Email                      EmptyString            `xml:"email"`
-	HomePhone                  EmptyString            `xml:"homephone"`
-	MobilePhone                EmptyString            `xml:"mobilephone"`
-	LaCountry                  Citizenship            `xml:"la_country"`
-	LaIndex                    EmptyString            `xml:"la_index"`
-	LaRegion                   EmptyString            `xml:"la_region"`
-	LaDistrict                 EmptyString            `xml:"la_district"`
-	LaCity                     EmptyString            `xml:"la_city"`
-	LaSettlement               EmptyString            `xml:"la_settlement"`
-	LaStreet                   EmptyString            `xml:"la_street"`
-	LaHouse                    EmptyString            `xml:"la_house"`
-	LaBuilding                 EmptyString            `xml:"la_building"`
-	LaStructure                EmptyString            `xml:"la_structure"`
-	LaApartment                EmptyString            `xml:"la_apartment"`
-	LaYears                    EmptyString            `xml:"la_years"`
-	LaMonth                    EmptyString            `xml:"la_month"`
-	LaDate                     Date                   `xml:"la_date"`
-	RaPhone                    EmptyString            `xml:"ra_phone"`
-	RaCountry                  Citizenship            `xml:"ra_country"`
-	RaIndex                    EmptyString            `xml:"ra_index"`
-	RaRegion                   EmptyString            `xml:"ra_region"`
-	RaDistrict                 EmptyString            `xml:"ra_district"`
-	RaCity                     EmptyString            `xml:"ra_city"`
-	RaSettlement               EmptyString            `xml:"ra_settlement"`
-	RaStreet                   EmptyString            `xml:"ra_street"`
-	RaHouse                    EmptyString            `xml:"ra_house"`
+	DocPlace          EmptyString            `xml:"docplace"`
+	DocDate           Date                   `xml:"docdate"`
+	DocCode           EmptyString            `xml:"doccode"`
+	PastDocType       DocType                `xml:"pastdoctype"`
+	PastDocNo         EmptyString            `xml:"pastdocno"`
+	PastDocPlace      EmptyString            `xml:"pastdocplace"`
+	PastDocDate       Date                   `xml:"pastdocdate"`
+	Sex               Sex                    `xml:"sex"`
+	Citizenship       Country            `xml:"citizenship"`
+	INN               EmptyString            `xml:"inn"`
+	PFR               EmptyString            `xml:"pfr"`
+	DriverNo          EmptyString            `xml:"driverno"`
+	Education         Education              `xml:"education"`
+	Marital           Marital                `xml:"marital"`
+	NumChildren       EmptyString            `xml:"numchildren"`
+	Email             EmptyString            `xml:"email"`
+	HomePhone         EmptyString            `xml:"homephone"`
+	MobilePhone       EmptyString            `xml:"mobilephone"`
+	LaCountry         Country            `xml:"la_country"`
+	LaIndex           EmptyString            `xml:"la_index"`
+	LaRegion          EmptyString            `xml:"la_region"`
+	LaDistrict        EmptyString            `xml:"la_district"`
+	LaCity            EmptyString            `xml:"la_city"`
+	LaSettlement      EmptyString            `xml:"la_settlement"`
+	LaStreet          EmptyString            `xml:"la_street"`
+	LaHouse           EmptyString            `xml:"la_house"`
+	LaBuilding        EmptyString            `xml:"la_building"`
+	LaStructure       EmptyString            `xml:"la_structure"`
+	LaApartment       EmptyString            `xml:"la_apartment"`
+	LaYears           EmptyString            `xml:"la_years"`
+	LaMonth           EmptyString            `xml:"la_month"`
+	LaDate            Date                   `xml:"la_date"`
+	RaPhone           EmptyString            `xml:"ra_phone"`
+	RaCountry         Country            `xml:"ra_country"`
+	RaIndex           EmptyString            `xml:"ra_index"`
+	RaRegion          EmptyString            `xml:"ra_region"`
+	RaDistrict        EmptyString            `xml:"ra_district"`
+	RaCity            EmptyString            `xml:"ra_city"`
+	RaSettlement      EmptyString            `xml:"ra_settlement"`
+	RaStreet          EmptyString            `xml:"ra_street"`
+	RaHouse           EmptyString            `xml:"ra_house"`
 	RaBuilding                 EmptyString            `xml:"ra_building"`
 	RaStructure                EmptyString            `xml:"ra_structure"`
 	RaApartment                EmptyString            `xml:"ra_apartment"`
@@ -89,21 +89,21 @@ type NewApplication struct {
 	EmployerSize               EmployerSize           `xml:"employersize"`
 	BusinessIndustry           BusinessIndustry       `xml:"businessindustry"`
 	Position                   EmptyString            `xml:"position"`
-	EmploymentYear             EmptyString            `xml:"employment_year"`
-	EmploymentMonth            EmptyString            `xml:"employment_month"`
-	EmploymentDate             Date                   `xml:"employment_date"`
-	EmploymentINN              EmptyString            `xml:"employment_inn"`
-	IncomeProof                IncomeProof            `xml:"incomeproof"`
-	MonthlyIncome              EmptyString            `xml:"monthlyincome"`
-	BaPhone                    EmptyString            `xml:"ba_phone"`
-	BaCountry                  Citizenship            `xml:"ba_country"`
-	BaIndex                    EmptyString            `xml:"ba_index"`
-	BaRegion                   EmptyString            `xml:"ba_region"`
-	BaDistrict                 EmptyString            `xml:"ba_district"`
-	BaCity                     EmptyString            `xml:"ba_city"`
-	BaSettlement               EmptyString            `xml:"ba_settlement"`
-	BaStreet                   EmptyString            `xml:"ba_street"`
-	BaHouse                    EmptyString            `xml:"ba_house"`
+	EmploymentYear    EmptyString            `xml:"employment_year"`
+	EmploymentMonth   EmptyString            `xml:"employment_month"`
+	EmploymentDate    Date                   `xml:"employment_date"`
+	EmploymentINN     EmptyString            `xml:"employment_inn"`
+	IncomeProof       IncomeProof            `xml:"incomeproof"`
+	MonthlyIncome     EmptyString            `xml:"monthlyincome"`
+	BaPhone           EmptyString            `xml:"ba_phone"`
+	BaCountry         Country            `xml:"ba_country"`
+	BaIndex           EmptyString            `xml:"ba_index"`
+	BaRegion          EmptyString            `xml:"ba_region"`
+	BaDistrict        EmptyString            `xml:"ba_district"`
+	BaCity            EmptyString            `xml:"ba_city"`
+	BaSettlement      EmptyString            `xml:"ba_settlement"`
+	BaStreet          EmptyString            `xml:"ba_street"`
+	BaHouse           EmptyString            `xml:"ba_house"`
 	BaBuilding                 EmptyString            `xml:"ba_building"`
 	BaStructure                EmptyString            `xml:"ba_structure"`
 	BaApartment                EmptyString            `xml:"ba_apartment"`
@@ -115,21 +115,21 @@ type NewApplication struct {
 	DownPaymentAmount          EmptyString            `xml:"downpaymentamount"`
 	CollateralExistence        CollateralExistence    `xml:"collateralexistence"`
 	CollateralValue            EmptyString            `xml:"collateralvalue"`
-	PurchaseExistence          PurchaseExistence      `xml:"purchaseexistence"`
-	PurchaseValue              EmptyString            `xml:"purchasevalue"`
-	PurchaseModel              EmptyString            `xml:"purchasemodel"`
-	OperatorCode               EmptyString            `xml:"operator_code"`
-	OperatorName               EmptyString            `xml:"operator_name"`
-	PosCode                    EmptyString            `xml:"pos_code"`
-	PosPhone                   EmptyString            `xml:"pos_phone"`
-	PosCountry                 Citizenship            `xml:"pos_country"`
-	PosIndex                   EmptyString            `xml:"pos_index"`
-	PosRegion                  EmptyString            `xml:"pos_region"`
-	PosDistrict                EmptyString            `xml:"pos_district"`
-	PosCity                    EmptyString            `xml:"pos_city"`
-	PosSettlement              EmptyString            `xml:"pos_settlement"`
-	PosStreet                  EmptyString            `xml:"pos_street"`
-	PosHouse                   EmptyString            `xml:"pos_house"`
+	PurchaseExistence PurchaseExistence      `xml:"purchaseexistence"`
+	PurchaseValue     EmptyString            `xml:"purchasevalue"`
+	PurchaseModel     EmptyString            `xml:"purchasemodel"`
+	OperatorCode      EmptyString            `xml:"operator_code"`
+	OperatorName      EmptyString            `xml:"operator_name"`
+	PosCode           EmptyString            `xml:"pos_code"`
+	PosPhone          EmptyString            `xml:"pos_phone"`
+	PosCountry        Country            `xml:"pos_country"`
+	PosIndex          EmptyString            `xml:"pos_index"`
+	PosRegion         EmptyString            `xml:"pos_region"`
+	PosDistrict       EmptyString            `xml:"pos_district"`
+	PosCity           EmptyString            `xml:"pos_city"`
+	PosSettlement     EmptyString            `xml:"pos_settlement"`
+	PosStreet         EmptyString            `xml:"pos_street"`
+	PosHouse          EmptyString            `xml:"pos_house"`
 	PosBuilding                EmptyString            `xml:"pos_building"`
 	PosStructure               EmptyString            `xml:"pos_structure"`
 	PosApartment               EmptyString            `xml:"pos_apartment"`
